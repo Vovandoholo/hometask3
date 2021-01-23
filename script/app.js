62f2741c1965521b9dbbf250c70dcffb3d768aba
Vovandoholo <>
2021-01-23 21:33:16 +0200

    ready with 3,4,5 tasks

--- script/app.js
+++ script/app.js
@@ -33,55 +33,52 @@ console.log(getArraysNotEqualElementsCount([5, 6, 7, 8], [5, 6, 7, 8]));
 // то удаляем его с помощью filter и потом count++, это если if, а если else то просто вернуть текущий результат
 //нужно находить индекс, вместо find делать findIndex, если Index найден, то необходимо удалить элемент по индексу
 
-
-
 function getArraysEqualElementsCountHard(arr1, arr2) {
-  let count = 0;
-  arr1.reduce(function (result, item) {
-    let lookingForElement = result.findIndex(function(el){
+  let length = arr2.length;
+    arr1.reduce(function (result, item) {
+
+    let lookingForElement = result.findIndex(function(el) {
       return el ===item;
     })
     if (lookingForElement !== -1) {
       result.splice (lookingForElement,1);
-      count++
     }
     return result;
   },arr2);
-return count;
+return length - arr2.length ;
 }
 
 console.log(getArraysEqualElementsCountHard([1, 1, 1, 'e', 'e', 'd', 'e', 'e', 'd'], [1,1,1,1,1,1,1,1,1,1,'e','a', 'b', 'c', 'd', 'e']));
 
-
-
 // 4.getArrayElementsInARowAmount, которая возвращает количество раз, когда встретились два одинаковых элемента подряд.
 function getArrayElementsInARowAmount(arr) {
-    let count = 0;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] === arr[i - 1]) {
-            count++;
-        }
-    }
-    return count;
+  let count = 0;
+  arr.reduce(function(result, item, i) {
+// debugger
+    if (item === arr[i - 1]) {
+      count++;
+    };
+    return result;
+  },arr);
+  return count;
 }
-console.log(getArrayElementsInARowAmount([1, 2, 1, 2]))
+console.log(getArrayElementsInARowAmount([1, 1, 2, 2, 3, 4, 5, 5, 6, 7, 7]))
 
 // 5.getArrayElementByType, которая принимает 2 параметра - массив и строку, строка  указывает тип элементов, которые должны
 //остаться в массиве
 
 function getArrayElementByType(arr, str) {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof(arr[i]) === str) {
-            newArr.push(arr[i]);
+  return arr.reduce(function(result,item) {
+    if (typeof(item) === str) {
+      result = result.filter(function(el){
+        if (typeof(el) === str) {
+          return true
         }
+      });
     }
-    return newArr;
+    return result;
+  },arr);
 }
 console.log(getArrayElementByType([1, 'asd', 2, true, false, true, 3], 'boolean'))
 console.log(getArrayElementByType([1, 'asd', 2, true, false, true, 3], 'string'))
 console.log(getArrayElementByType([1, 'asd', 2, true, false, true, 3], 'number'))
-
-
-
-*/
